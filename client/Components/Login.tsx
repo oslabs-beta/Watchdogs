deca4ac24ae7f7328a313e18e2b91ca5bb41dc27
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2023-04-12 10:01:33 -0400

    Moved particles json into seperate folder

--- client/Components/Login.tsx
+++ client/Components/Login.tsx
@@ -5,125 +5,16 @@ import { loadFull } from 'tsparticles';
 import logo from '../assets/watchdogs-black.png';
 import '../scss/Login.scss'
 import { Engine } from 'tsparticles-engine';
+import loginParticles from '../assets/login-particles.json'
 
 function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const navigate = useNavigate()
-    const options: any = {
-        "particles": {
-          "number": {
-            "value": 80,
-            "density": {
-              "enable": true,
-              "value_area": 800
-            }
-          },
-          "color": {
-            "value": "#d98918"
-          },
-          "shape": {
-            "type": "circle",
-            "stroke": {
-              "width": 4,
-              "color": "#000000"
-            },
-            "polygon": {
-              "nb_sides": 5
-            },
-            "image": {
-              "src": "img/github.svg",
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 0.8996283114607908,
-            "random": false,
-            "anim": {
-              "enable": false,
-              "speed": 1,
-              "opacity_min": 0.1,
-              "sync": false
-            }
-          },
-          "size": {
-            "value": 3.945738208161363,
-            "random": true,
-            "anim": {
-              "enable": false,
-              "speed": 40,
-              "size_min": 0.1,
-              "sync": false
-            }
-          },
-          "line_linked": {
-            "enable": true,
-            "distance": 150,
-            "color": "#ffffff",
-            "opacity": 0.4,
-            "width": 1
-          },
-          "move": {
-            "enable": true,
-            "speed": 6.413648243462092,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-              "enable": false,
-              "rotateX": 600,
-              "rotateY": 1200
-            }
-          }
-        },
-        "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-            "onhover": {
-              "enable": true,
-              "mode": "repulse"
-            },
-            "onclick": {
-              "enable": true,
-              "mode": "push"
-            },
-            "resize": true
-          },
-          "modes": {
-            "grab": {
-              "distance": 400,
-              "line_linked": {
-                "opacity": 1
-              }
-            },
-            "bubble": {
-              "distance": 596.8428232695622,
-              "size": 227.36869457888088,
-              "duration": 1.5428589989281203,
-              "opacity": 0.6983467047779912,
-              "speed": 3
-            },
-            "repulse": {
-              "distance": 200,
-              "duration": 0.4
-            },
-            "push": {
-              "particles_nb": 4
-            },
-            "remove": {
-              "particles_nb": 2
-            }
-          }
-        },
-        "retina_detect": true
-      }
     const particlesInit = useCallback(async (engine: Engine) => {
-    await loadFull(engine);
+      await loadFull(engine);
     }, []);
-
+    const options:any = loginParticles
 
     function loginSubmit (e: React.MouseEvent<HTMLButtonElement>) {
         e.preventDefault()
