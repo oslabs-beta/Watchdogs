7f9056f54e86c78ae9aeeccf0dd8dff932fdacc2
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2023-04-12 14:52:21 -0400

    Completed test suite for backend

--- __tests__/database.test.ts
+++ __tests__/database.test.ts
@@ -99,49 +99,125 @@ describe ('Testing Suite for Routes', ()=> {
     
   })
   
-    xdescribe ('GET /user', () => {
+    describe ('GET /user', () => {
 
-      it('Responds with a 200 status code', () => {
-        
-      })
-  
-      it('Responds with a JSON object', () => {
-  
-      })
+      let userId:string;
 
-      it('Responds with the users data', () => {
-        
+      beforeAll(() => request(server)
+      .post('/api/signup')
+      .send({username: testUser, password: testPass, arn: testArn})
+      .then(response => {
+        userId = response.body.user._id
+        })
+      )
+
+      afterAll(() => request(server)
+      .delete('/api/user')
+      .send({username: testUser})
+      )
+
+      it('Responds with a 200 status code', () => request(server)
+      .get('/api/user')  
+      .set('Cookie', `userId=${userId}`)
+      .expect(200)
+      )
+  
+      it('Responds with a JSON object', () => request(server)
+      .get('/api/user')  
+      .set('Cookie', `userId=${userId}`)
+      .then(response => {
+        expect(response.type).toEqual('application/json')
       })
+      )
+
+      it('Responds with the users data', () => request(server)
+      .get('/api/user')  
+      .set('Cookie', `userId=${userId}`)
+      .then(response => {
+        expect(response.body.username).toEqual(testUser)
+        expect(response.body.password).not.toEqual(testPass)
+        expect(response.body.arn).toEqual(testArn)
+        })
+      )
 
   })
 
-    xdescribe ('PUT /user', () => {
+    describe ('PUT /user', () => {
 
-      it('Responds with a 200 status code', () => {
-        
+      beforeAll(() => request(server)
+      .post('/api/signup')
+      .send({username: testUser, password: testPass, arn: testArn})
+      )
+
+      afterAll(() => request(server)
+      .delete('/api/user')
+      .send({username: testUser})
+      )
+
+      it('Responds with a 200 status code', () => request(server)
+      .put('/api') 
+      .send({username: testUser, arn: testArn}) 
+      .expect(200)
+      )
+
+      it('Responds with a JSON object', () => request(server)
+      .put('/api') 
+      .send({username: testUser, arn: testArn}) 
+      .then(response => {
+        expect(response.type).toEqual('application/json')
       })
+      )
 
-      it('Responds with a JSON object', () => {
-
-      })
-
-      it('Responds with the updated users data', () => {
-        
-      })
+      it('Responds with the updated users data', () => request(server)
+      .put('/api')
+      .send({username: testUser, arn: 'updatedArn'})
+      .then(response => {
+        expect (response.body.username).toEqual(testUser);
+        expect(response.body.arn).not.toEqual(testArn)
+        expect(response.body.arn).toEqual('updatedArn')
+      })   
+      )
 
   })
-  xdescribe ('DELETE /user', () => {
-    it ('Responds with a 200 status code', () => {
+  describe ('DELETE /user', () => {
 
+    let userId:string;
+
+
+    beforeEach(() => request(server)
+    .post('/api/signup')
+    .send({username: testUser, password: testPass, arn: testArn})
+    .then(response => {
+      userId = response.body.user._id
+      })
+    )
+
+    it ('Responds with a 200 status code', () => request(server)
+    .delete('/api/user')
+    .send({username: testUser})
+    .expect(200)
+
+    )
+    it('Responds with a JSON object', () => request(server)
+    .delete('/api/user')
+    .send({username: testUser})
+    .then(response => {
+      expect(response.type).toEqual('application/json')
     })
-    it('Responds with a JSON object', () => {
 
-    })
+    )
 
-    it('Responds with the deleted users data', () => {
-      
-    })
+    it('Responds with the deleted users data', () => request(server)
+    .delete('/api/user')
+    .send({username: testUser})
+    .then(response => {
+      expect(response.body.username).toEqual(testUser)
+      expect(response.body.password).not.toEqual(testPass)
+      expect(response.body.arn).toEqual(testArn)
+      })  
+    )
 
+   
   })
   
 })
