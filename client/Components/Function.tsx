b30a9e98381c52b16607e6ed650533bca5b10ba0
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2023-04-19 16:58:49 -0400

    Added spreadsheet export capability for error logs

--- client/Components/Function.tsx
+++ client/Components/Function.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import Chart from './Chart';
 import { jsPDF } from 'jspdf';
+import Excel from 'exceljs'
+import * as FileSaver from 'file-saver';
 import { useState, useEffect } from 'react'
 
 // Type Imports
@@ -63,13 +65,53 @@ function Function(props: FunctionProps) {
     })
   }
 
+  const generateSpreadsheet = async (): Promise<void> => {
+    fetch ('/api/error', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            region: user.region,
+            arn: user.arn,
+            func: functionName
+          } as ErrorBodyType),
+        })
+        .then((res): Promise<ErrorData[]> => res.json())
+        .then((res) => {
+          const workbook = new Excel.Workbook();
+          const worksheet = workbook.addWorksheet("My Sheet");
+          const blobType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+
+          worksheet.columns = [
+            {header: '#', key: 'id', width: 10},
+            {header: 'Log Stream Name', key: 'logStreamName', width: 32}, 
+            {header: 'Timestamp', key: 'timestamp', width: 15,},
+            {header: 'Message', key: 'message', width: 64},
+            {header: 'Ingestion Time', key: 'ingestionTime', width: 15},
+            {header: 'Event ID', key: 'eventId', width: 32}         
+          ]
+          for (let i = 0; i < res.length; i++){
+            worksheet.addRow({id: `${i + 1}`, logStreamName: res[i].logStreamName, timestamp: new Date(res[i].timestamp!).toString(), message: res[i].message, ingestionTime: new Date(res[i].ingestionTime!).toString(), eventId: res[i].eventId})
+          }
+          // save under export.xlsx
+          workbook.xlsx.writeBuffer().then(data => {
+              const blob = new Blob([data], { type: blobType }); 
+              FileSaver.saveAs(blob, `${functionName}ErrorSpreadsheet`);
+              })        
+        })
+  }
   // Render Component
   return (
     <>
       <div className='function' id={functionName}>
         <h2>&quot;{functionName}&quot;</h2>
         <Chart key={functionName} data={functionData} />
-        <button className='error-button' onClick={() => generatePDF()}>Generate Error Logs</button>
+        <div>
+          <p>Generate Error Logs: </p>
+          <button className='error-button' onClick={() => generatePDF()}>As PDF</button>
+          <button className='error-button' onClick={() => generateSpreadsheet()} >As Spreadsheet</button>
+        </div>
       </div>
     </>
   );
