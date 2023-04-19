5b0fa4bfd144e99fa43eedc048bee398ed95afff
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2023-04-19 15:53:39 -0400

    Error logs are fit to page, function border dynamically colored to errors.

--- client/Components/Function.tsx
+++ client/Components/Function.tsx
@@ -1,18 +1,28 @@
 // React Imports
 import React from 'react';
 import Chart from './Chart';
-import { jsPDF } from 'jspdf'
-
+import { jsPDF } from 'jspdf';
+import { useState, useEffect } from 'react'
 
 // Type Imports
 import { FunctionProps, ErrorData , ErrorBodyType} from '../types';
-//region, arn, function name
+
+
 // Main Function
 function Function(props: FunctionProps) {
   // Destructure Props
   const { functionData, functionName, user } = props;
-
-
+  //change function border to red if there are errors
+  useEffect(() => {
+    if (Math.max(...functionData.Errors.values)) {
+        const errorFunc = document.getElementById(functionName);
+        if ( errorFunc){
+          errorFunc.style.border = 'solid 1px red'
+        }
+    }
+  }, [])
+ 
+  // PDF Generator Function
   const generatePDF = (): void => {
     fetch ('/api/error', {
       method: 'POST',
@@ -28,30 +38,43 @@ function Function(props: FunctionProps) {
     .then((res): Promise<ErrorData[]> => res.json())
     .then((res) => {
       const doc = new jsPDF();
-      const output = res.map(error => doc.splitTextToSize(`${error.message}: ${error.timestamp}`, 50))
-      // res.forEach((el) => {
-        
-      //   const split = doc.splitTextToSize(`${el.message} ${el.timestamp}`, 50)
-      //   output.push(split)
-      // })
-      // const errors: string[]
-      // const log: string = doc.splitTextToSize(errors, 50)
-      console.log(output)
-      doc.text(output, 10, 10)     
+      doc.setFontSize(12);
+      const output: string[] = []
+      res.forEach((error) => {
+        if (error.message && error.timestamp){
+          output.push(`${new Date(error.timestamp).toString()}: `)
+          const cut = error.message.slice(68);
+          const splitMessage = doc.splitTextToSize(cut, 190)
+          splitMessage.forEach((split: string) => output.push(split))
+        }
+      })
+      const pageHeight  = doc.internal.pageSize.getHeight()
+      let y = 10;
+      output.forEach(el => {
+        doc.text(el, 10, y);
+        y += 5;
+        if (y > pageHeight) {
+          doc.addPage();
+          y = 10;
+        }
+      })
 
       doc.save(`${functionName}ErrorLogs.pdf`)
     })
   }
+
   // Render Component
   return (
     <>
-      <div className="function">
+      <div className='function' id={functionName}>
         <h2>&quot;{functionName}&quot;</h2>
-        <button onClick={() => generatePDF()}>Generate Error Logs</button>
         <Chart key={functionName} data={functionData} />
+        <button className='error-button' onClick={() => generatePDF()}>Generate Error Logs</button>
       </div>
     </>
   );
 }
 
 export default Function;
+
+
