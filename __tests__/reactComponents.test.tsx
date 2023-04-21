189207d63e0027feeb3988e677ace888bf4257de
Weston Clark <westoclark@gmail.com>
2023-04-21 09:21:14 -0700

    React testing library setup first 3 tests made

--- __tests__/reactComponents.test.tsx
+++ __tests__/reactComponents.test.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { fireEvent, render, waitFor, screen } from '@testing-library/react';
+import { fireEvent, render, RenderResult, waitFor, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import App from '../client/App';
 import Login from '../client/Components/Login';
 import Signup from '../client/Components/Signup';
 
 describe('React Component Testing', () => {
+  let app: RenderResult;
   describe('SignUp Page', () => {
-    test('Renders a signup container', () => {
-      render(<App />);
-      const linkElement = screen.getByTestId('signup-container');
-      expect(linkElement).toBeInTheDocument;
+    beforeEach(() => {
+      app = render(
+        <MemoryRouter>
+          <Signup />
+        </MemoryRouter>
+      );
     });
 
-    test('Should render a Username and Password fields', () => {});
+    test('Renders a signup container', () => {
+      const linkElement = app.container.querySelector('#signup-container');
+      expect(linkElement).toBeInTheDocument();
+    });
 
-    test('Should render a submit button', () => {});
+    test('Should render a Username and Password fields', () => {
+      const usernameElement = app.container.querySelector('#username-field');
+      const passwordElement = app.container.querySelector('#password-field');
+      expect(usernameElement).toBeInTheDocument();
+      expect(passwordElement).toBeInTheDocument();
+    });
 
-    test("Should display 'Username already exists' if username already exists", () => {});
+    test('Should render a submit button', () => {
+      const signupButton = app.getByRole('button', {
+        name: 'Signup',
+      });
+      expect(signupButton).toBeInTheDocument();
+    });
 
-    test("Should redirect to '/Home' on succsessful signup", () => {});
+    // test("Should display 'Username already exists' if username already exists", () => {});
+
+    // test("Should redirect to '/Home' on succsessful signup", () => {});
   });
 
-  describe('Login Page', () => {
+  xdescribe('Login Page', () => {
     test('Should render a login container', () => {});
 
     test('Should render Username and Password fields', () => {});
@@ -35,7 +54,7 @@ describe('React Component Testing', () => {
     test("Should redirect to '/Home' on sucsessfull login", () => {});
   });
 
-  describe('Home Page', () => {
+  xdescribe('Home Page', () => {
     test('Should render navbarer component', () => {});
 
     test('Should render a Functions Container', () => {});
@@ -53,3 +72,34 @@ describe('React Component Testing', () => {
     });
   });
 });
+
+// function renderLoginForm(props: Partial<Props> = {}) {
+//   // const defaultProps: Props = {
+//   //   onPasswordChange() {
+//   //     return;
+//   //   },
+//   //   onRememberChange() {
+//   //     return;
+//   //   },
+//   //   onUsernameChange() {
+//   //     return;
+//   //   },
+//   //   onSubmit() {
+//   //     return;
+//   //   },
+//   //   shouldRemember: true,
+//   // };
+//   return render(<LoginForm {...defaultProps} {...props} />);
+// }
+
+// test('should display a blank login form, with remember me checked by default', async () => {
+//   const { findByTestId } = renderLoginForm();
+
+//   const loginForm = await findByTestId('login-form');
+
+//   expect(loginForm).toHaveFormValues({
+//     username: '',
+//     password: '',
+//     remember: true,
+//   });
+// });
